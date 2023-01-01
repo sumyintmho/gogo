174427b06f1d68f7cb94c9af00b6f4b365d2e65f
sumyintmho <mhosu621@gmail.com>
2023-01-01 18:55:45 +0630

    wait

---
+++ app.js
@@ -0,0 +1,28 @@
+
+    document.getElementById("btn1").addEventListener('click',function(){
+        document.getElementById("squ3").style.background = "#767787";
+    });
+
+    document.getElementById("btn1").addEventListener('dblclick',function(){
+        document.getElementById("squ3").style.background = "rgb(66, 103, 83)";
+    });
+
+    document.getElementById("btn2").addEventListener('click',function(){
+        document.getElementById("btn2").innerHTML="Order Now";
+    });
+
+    document.getElementById("btn3").addEventListener('click',function(){
+        document.getElementById("btn3").innerHTML="Order Now";
+    });
+
+    document.getElementById("btn4").addEventListener('click',function(){
+        document.getElementById("btn4").innerHTML="Order Now";
+    });
+
+    document.getElementById("btn5").addEventListener('click',function(){
+        document.getElementById("btn5").innerHTML="Order Now";
+    });
+
+
+
+    
